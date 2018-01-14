50318c44e202777f00782fd1e1b08a45b4640156
Julien Melissas <julien.melissas@gmail.com>
2018-01-13 20:02:05 -0500

    Update updraftplus-hourly.php
    Fixed updraft issue saying there was extra whitespace

--- updraftplus-hourly.php
+++ updraftplus-hourly.php
@@ -1,32 +1,28 @@
-
 <?php
-/**
- * Plugin Name:       UpdraftPlus Hourly
- * Plugin URI:        https://github.com/Craftpeak/updraftplus-hourly/
- * Description:       Adds the hourly interval to UpdraftPlus
- * Version:           1.0.0
- * Author:            Craftpeak
- * Author URI:        https://craftpeak.com/
- * Text Domain:       cp
- * Domain Path:       /lang
- */
+/*
+Plugin Name:       UpdraftPlus Hourly
+Plugin URI:        https://github.com/Craftpeak/updraftplus-hourly/
+Description:       Adds the hourly interval to UpdraftPlus
+Version:           1.0.0
+Author:            Craftpeak
+Author URI:        https://craftpeak.com/
+Text Domain:       cp
+Domain Path:       /lang
+*/
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-/**
- * Add the hourly interval to UpdraftPlus
- *
- * @link: https://updraftplus.com/faqs/how-can-i-add-any-new-scheduling-interval-to-updraftplus/
- */
+// Add the hourly interval to UpdraftPlus
+// (https://updraftplus.com/faqs/how-can-i-add-any-new-scheduling-interval-to-updraftplus/)
 add_filter( 'updraftplus_backup_intervals', function( $ints ) {
 	$ints['hourly'] = 'Hourly';
 	return $ints;
 } );
 
 add_filter( 'cron_schedules', function( $schedules ) {
-	$schedules['hourly'] = array( 'interval' => 3600, 'display' => 'Once Hourly' );
+	$schedules['hourly'] = [ 'interval' => 3600, 'display' => 'Once Hourly' ];
 	return $schedules;
-}, 31 );
+}, 99 );
